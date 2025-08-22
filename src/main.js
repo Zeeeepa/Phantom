b45ee5609181e898ac072ab046deee2a262737e2
ATK_Phantom <158583494+Attack-Phantom@users.noreply.github.com>
2025-08-22 23:22:05 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -772,7 +772,7 @@ class ILoveYouTranslucent7 {
     }
 }
 
-const CURRENT_VERSION = 'v1.6.6'; // 请根据实际版本修改
+const CURRENT_VERSION = 'v1.6.7'; // 请根据实际版本修改
 
 function compareVersion(v1, v2) {
     const arr1 = v1.replace(/^v/, '').split('.').map(Number);
